a8b90678fe53e3ff5ba7321823d028dd981ab0a6
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 17:00:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  })
+}
+
+const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    return Object.assign({}, driver, {firstName: driver.split(' ')[0] , lastName: driver.split(' ')[1] });
+  })
+}
+
+nameToAttributes(drivers);
