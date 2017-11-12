7aaa3c5c18490d61cd8172bb76d57fee0bb9ce07
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 18:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map(function(driver)) {
+  return drivers.map(function(driver) {
     return `${driver.name} is from ${driver.hometown}`;
-  }
+  });
 }
