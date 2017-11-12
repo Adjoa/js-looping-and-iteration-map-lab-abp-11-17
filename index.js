e0990f6194d522fb3a5300b436c4ac65e29773a9
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 17:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ function lowerCaseDrivers(drivers) {
 //const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
 
 function nameToAttributes(drivers) {
-  drivers.forEach(
-    function(driver) {
-      return Object.assign({}, driver, {firstName: driver[0] , lastName: driver[1] });
-    }
-  );
+  //drivers.forEach(
+  //  function(driver) {
+  //    return Object.assign({}, driver, {firstName: driver[0] , lastName: driver[1] });
+  //  }
+  //);
+  
   //return drivers.map(function(driver) {
   //  return Object.assign({}, driver, {firstName: driver[0] , lastName: driver[1] });
   //})
