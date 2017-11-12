cca69c616147f71fc4fc4fd273a3f5cd5d6333bc
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 18:21:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawye
 // try: new substring at ' '
 function nameToAttributes(drivers) {
   return drivers.map(function(driver) {
-    return Object.assign({}, driver, { firstName: driver.toLowerCase(), lastName: driver.toLowerCase });
+    //name "Bobby Smith"
+    let fName = driver.split(' ')[0];
+    let lName = driver.split(' ')[1];
+    return {firstName: fName, lastName: lName};
+    //return Object.assign({}, driver, { firstName: driver.toLowerCase(), lastName: driver.toLowerCase });
+    // {firstName: "Bobby", lastName: "Smith"}
   })
 }
 
