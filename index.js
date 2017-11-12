305fa755c0c614c2b183a745c814f2dd1d7afa0b
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-12 18:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,19 +5,12 @@ function lowerCaseDrivers(drivers) {
   })
 }
 
-
-
-const drivers = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
-// try: new substring at ' '
 function nameToAttributes(drivers) {
   return drivers.map(function(driver) {
-    //name "Bobby Smith"
     let fName = driver.split(' ')[0];
     let lName = driver.split(' ')[1];
     return {firstName: fName, lastName: lName};
-    //return Object.assign({}, driver, { firstName: driver.toLowerCase(), lastName: driver.toLowerCase });
-    // {firstName: "Bobby", lastName: "Smith"}
   })
 }
 
-nameToAttributes(drivers);
+
